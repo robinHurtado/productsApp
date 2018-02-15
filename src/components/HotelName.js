2a63fbca96f3a33bb46ca209bf2da3487228efff
robinHurtado <robin_aven94@hotmail.com>
2018-02-15 16:34:34 -0500

    Add stars in the star filter component

--- src/components/HotelName.js
+++ src/components/HotelName.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HotelName = () => (
   <div className="card" style={{marginBottom: '1%'}}>
     <div className="card-body">
-      <h6>Nombre de hotel</h6>
+      <h4>Nombre de hotel</h4>
       <div className="input-group">
         <input type="text" className="form-control" placeholder="Ingrese nombre del hotel" />
         <button className="btn btn-primary" type="button">Aceptar</button>
--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import star from '../Assets/icons/filters/star_yellow.svg';
+
+const StarIcon = () => (
+  <img src={star} alt="star_icon" />
+);
 
 const Stars = () => (
   <div className="card">
     <div className="card-body">
-      <h3>Estrellas</h3>
-      <div className="input-group">
-        {
-          ["Todas las estrellas",5,4,3,2,1].map((elem) => (
-            <span key={elem}> <input type="checkbox" />{elem}</span>
-          ))
-        }
-      </div>
+      <h4>Estrellas</h4>
+      <div><input type="checkbox" />Todas las estrellas</div>
+      <div><input type="checkbox" /><StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
+      <div><input type="checkbox" /><StarIcon /><StarIcon /><StarIcon /><StarIcon /></div>
+      <div><input type="checkbox" /><StarIcon /><StarIcon /><StarIcon /></div>
+      <div><input type="checkbox" /><StarIcon /><StarIcon /></div>
+      <div><input type="checkbox" /><StarIcon /></div>
     </div>
   </div>
 );
