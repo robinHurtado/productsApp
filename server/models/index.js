f99289d54ac381afceb02703f26322cea0644260
robinHurtado <robin_aven94@hotmail.com>
2018-02-16 22:06:31 -0500

    Add favicon file

--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-// this file imports as many models as you want
-const User = require("../models/User"); 
-
-// es6 allows you simplify {user:user} to just {user}
-const db = {
-	User
-}
-
-module.exports = db;
